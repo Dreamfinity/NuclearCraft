346460e6d64018b60b9c3bf7d53306c928c61138
Tom Dodd <joedodd35@gmail.com>
2025-06-01 18:04:55 +0100

    Fix ore gen block counts

--- src/main/java/nc/worldgen/ore/OreGenerator.java
+++ src/main/java/nc/worldgen/ore/OreGenerator.java
@@ -4,7 +4,8 @@ import it.unimi.dsi.fastutil.ints.*;
 import nc.block.BlockMeta;
 import nc.enumm.MetaEnums;
 import nc.init.NCBlocks;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
@@ -23,8 +24,70 @@ public class OreGenerator implements IWorldGenerator {
 	
 	public static class WorldGenOre extends WorldGenMinable {
 		
+		public final double countParameter;
+		
 		public WorldGenOre(int meta) {
-			super(((BlockMeta<?>) NCBlocks.ore).getStateFromMeta(meta), ore_size[meta] + 2, new UniversalOrePredicate());
+			super(((BlockMeta<?>) NCBlocks.ore).getStateFromMeta(meta), (int) getCountParameter(ore_size[meta]), new UniversalOrePredicate());
+			countParameter = getCountParameter(ore_size[meta]);
+		}
+		
+		private static final double A = 0.853D, B = 0.148D, C = 3.551D;
+		
+		public static double getCountParameter(int count) {
+			return count <= 0 ? 0D : (count <= 6 ? Math.pow(count + 4, 0.75D) : (Math.pow(count, 1D / C) - A) / B);
+		}
+		
+		public boolean generate(World worldIn, Random rand, BlockPos position) {
+			float f = rand.nextFloat() * (float) Math.PI;
+			double d0 = (float) (position.getX() + 8) + MathHelper.sin(f) * (float) countParameter / 8.0F;
+			double d1 = (float) (position.getX() + 8) - MathHelper.sin(f) * (float) countParameter / 8.0F;
+			double d2 = (float) (position.getZ() + 8) + MathHelper.cos(f) * (float) countParameter / 8.0F;
+			double d3 = (float) (position.getZ() + 8) - MathHelper.cos(f) * (float) countParameter / 8.0F;
+			double d4 = position.getY() + rand.nextInt(3) - 2;
+			double d5 = position.getY() + rand.nextInt(3) - 2;
+			
+			for (int i = 0, count = (int) countParameter; i < count; ++i) {
+				float f1 = (float) i / (float) countParameter;
+				double d6 = d0 + (d1 - d0) * (double) f1;
+				double d7 = d4 + (d5 - d4) * (double) f1;
+				double d8 = d2 + (d3 - d2) * (double) f1;
+				double d9 = rand.nextDouble() * countParameter / 16.0D;
+				double d10 = (double) (MathHelper.sin((float) Math.PI * f1) + 1.0F) * d9 + 1.0D;
+				double d11 = (double) (MathHelper.sin((float) Math.PI * f1) + 1.0F) * d9 + 1.0D;
+				int j = MathHelper.floor(d6 - d10 / 2.0D);
+				int k = MathHelper.floor(d7 - d11 / 2.0D);
+				int l = MathHelper.floor(d8 - d10 / 2.0D);
+				int i1 = MathHelper.floor(d6 + d10 / 2.0D);
+				int j1 = MathHelper.floor(d7 + d11 / 2.0D);
+				int k1 = MathHelper.floor(d8 + d10 / 2.0D);
+				
+				for (int l1 = j; l1 <= i1; ++l1) {
+					double d12 = ((double) l1 + 0.5D - d6) / (d10 / 2.0D);
+					
+					if (d12 * d12 < 1.0D) {
+						for (int i2 = k; i2 <= j1; ++i2) {
+							double d13 = ((double) i2 + 0.5D - d7) / (d11 / 2.0D);
+							
+							if (d12 * d12 + d13 * d13 < 1.0D) {
+								for (int j2 = l; j2 <= k1; ++j2) {
+									double d14 = ((double) j2 + 0.5D - d8) / (d10 / 2.0D);
+									
+									if (d12 * d12 + d13 * d13 + d14 * d14 < 1.0D) {
+										BlockPos blockpos = new BlockPos(l1, i2, j2);
+										
+										IBlockState state = worldIn.getBlockState(blockpos);
+										if (state.getBlock().isReplaceableOreGen(state, worldIn, blockpos, predicate)) {
+											worldIn.setBlockState(blockpos, oreBlock, 2);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			return true;
 		}
 	}
 	
